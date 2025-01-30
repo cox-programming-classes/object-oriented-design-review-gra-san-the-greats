8c58c8c96c53b8c45c4b01f6a6752b6a749176b4
zhouwinsor <56124717+zhouwinsor@users.noreply.github.com>
2025-01-30 09:19:31 -0500

    class 1/30

--- CS8_FirstObjects/Models/Vector2D.cs
+++ CS8_FirstObjects/Models/Vector2D.cs
@@ -27,8 +27,8 @@ public class Vector2D
     /// <summary>
     /// Compute the Angle of this vector!
     /// </summary>
-    /// <exception cref="NotImplementedException">TODO: Complete this code to get rid of this exception.</exception>
-    public AngleMeasure Angle => throw new NotImplementedException("You need to implement this function.");
+    /// <exception cref="NotImplementedException"
+    public AngleMeasure Angle => new(Math.Atan2(Y, X));
     #endregion
     
     #region Constructors
